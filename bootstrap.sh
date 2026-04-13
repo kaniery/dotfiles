e06434402658e37755ddb1c88606efb7c15b2677
kaniery <133332961+kaniery@users.noreply.github.com>
2026-04-13 10:42:23 +0900

    Enhance bootstrap script with permission check
    Added permission check and improved git clone logic.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-sudo pacman -S --noconfirm git ansible
 
-if [ ! -d "~/mysetup" ]; then
-    git clone https://github.com/kaniery/dotfiles.git ~/mysetup
+# 1. 権限確認
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo"
 fi
 
-cd ~/mysetup
+echo "Installing Ansible..."
+sudo pacman -Sy --noconfirm git ansible
+
+hash -r
+
+REPO_URL="https://github.com/kaniery/dotfiles.git"
+DEST="$HOME/mysetup"
+
+if [ ! -d "$DEST" ]; then
+    git clone "$REPO_URL" "$DEST"
+else
+    cd "$DEST" && git pull
+fi
+
+cd "$DEST"
+echo "Running Ansible Playbook..."
 ansible-playbook local.yml
