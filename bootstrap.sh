6be620cf5fd29aeb15c099f95bca9c5b40382f3f
kaniery <133332961+kaniery@users.noreply.github.com>
2026-04-13 14:21:45 +0900

    Enhance bootstrap script with error handling
    Added error handling and improved output messages for clarity.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
+set -e
+
 if command -v sudo >/dev/null 2>&1; then
     SUDO="sudo"
 else
     SUDO=""
 fi
 
-echo "Installing Ansible..."
+echo "--- 1. Installing dependencies ---"
 
 $SUDO pacman -Sy --noconfirm git ansible
 
+
 hash -r
 
-DEST="$HOME/dotfiles"
-if [ ! -d "$DEST/.git" ]; then
-    git clone https://github.com/kaniery/dotfiles.git "$DEST"
-else
-    cd "$DEST" && git pull
-fi
+echo "--- 2. Checking ansible-playbook ---"
 
-cd "$DEST"
-echo "Running Ansible Playbook..."
+which ansible-playbook
+
+echo "--- 3. Running Ansible Playbook ---"
+
 ansible-playbook local.yml
