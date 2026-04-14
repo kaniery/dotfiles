5bf6d10cad86d34a21292c78570108dd48f5675f
kaniery <133332961+kaniery@users.noreply.github.com>
2026-04-14 09:56:44 +0900

    Refactor bootstrap.sh for improved output
    Updated echo statements for clarity and removed unnecessary checks.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 if command -v sudo >/dev/null 2>&1; then
@@ -12,13 +11,8 @@ echo "--- 1. Installing dependencies ---"
 
 $SUDO pacman -Sy --noconfirm git ansible
 
-
 hash -r
 
-echo "--- 2. Checking ansible-playbook ---"
-
-which ansible-playbook
-
-echo "--- 3. Running Ansible Playbook ---"
+echo "--- ２. Running Ansible Playbook ---"
 
 ansible-playbook local.yml
